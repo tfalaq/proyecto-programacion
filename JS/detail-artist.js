246061c91e9595d55d69811615a666455ecdd99a
macszim <malfonso@udesa.edu.ar>
2021-06-16 10:36:28 -0300

    mod html,js detail artist y css

--- JS/detail-artist.js
+++ JS/detail-artist.js
@@ -1,36 +1,67 @@
-window.addEventListener('load', function(){
-
-    let urlArtist=`https://cors-anywhere.herokuapp.com/https://api.deezer.com/artist/`
-    async function RealizarPeticion(url,mensajeError){
-        try {
-          let response=await fetch(url)
-          let respuesta=await response.json()
-          return respuesta
-        } catch (error) {
-            console.log(`Ocurrio un error en ${mensajeError}, ${error}`);
-        }
-      }
-
-    function leerQueryString(){
-        //obtiene la queryString Completa
-        const querystring = window.location.search
-        console.log(querystring);
-        //convierte la query string en objeto
-        const params = new URLSearchParams(querystring)
-        console.log(params);
-        let id=params.get('Id')
-        return id
+window.addEventListener('load', function () {
+  let urlArtist = `https://cors-anywhere.herokuapp.com/https://api.deezer.com/artist/`
+  async function RealizarPeticion(url, mensajeError) {
+    try {
+      let response = await fetch(url)
+      let respuesta = await response.json()
+      return respuesta
+    } catch (error) {
+      console.log(`Ocurrio un error en ${mensajeError}, ${error}`);
     }
+  }
 
-   async function BuscarArtista(){
-       let respuesta=await RealizarPeticion(`${urlArtist}${leerQueryString()}`,'DetailArtist')
-       let titulo=document.createElement('h2');
-       let imagenArista=document.createElement('img');
-       imagenArista.src=respuesta.picture_medium
-       document.querySelector('main').appendChild(titulo)
-       document.querySelector('main').appendChild(imagenArista)
-   }
-   BuscarArtista()
+  function leerQueryString() {
+    //obtiene la queryString Completa
+    const querystring = window.location.search
+    console.log(querystring);
+    //convierte la query string en objeto
+    const params = new URLSearchParams(querystring)
+    console.log(params);
+    let id = params.get('Id')
+    return id
+  }
 
-    
+  async function BuscarArtista() {
+    let respuesta = await RealizarPeticion(`${urlArtist}${leerQueryString()}`, 'DetailArtist')
+    let albums = await RealizarPeticion(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/artist/${respuesta.id}/albums?limit=5`)
+     CreaPlayList(albums.data)
+    let imagenArtista = document.querySelector('.card-Artist img')
+    imagenArtista.src = respuesta.picture_xl
+    let detallesArtista = document.querySelector('.Subtitle-detail b')
+    detallesArtista.innerHTML = respuesta.name
+
+
+    console.log(albums);
+  }
+
+  function CreaPlayList(albums) {
+    albums.map(album=>{
+      let article=document.createElement('article');
+      article.classList.add('card')
+      let infoCard=document.createElement('div')
+      infoCard.classList.add('informacion')
+      infoCard.classList.add('details')
+      let title=document.createElement('h1')
+      title.innerText=album.title
+      let img=document.createElement('img')
+      img.src=album.cover_big
+      infoCard.appendChild(title)
+      infoCard.appendChild(img)
+      article.appendChild(infoCard)
+
+      document.querySelector('#topTracks').appendChild(article)
+
+      // <article class="card">
+      //       <div class="informacion details">
+      //           <h1>Yonaguni</h1>
+      //           <img src="https://cdns-images.dzcdn.net/images/cover/3aa544b9653b10aedcf7eb41d61b22df/56x56-000000-80-0-0.jpg" alt="" srcset="">
+      //       </div>
+      //     </article>
+    })
+  }
+
+
+
+
+  BuscarArtista()
 })
--- JS/index.js
+++ JS/index.js
@@ -1,42 +1,40 @@
-let topTracksUrl = 'https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart/0/tracks';
-let topArtistUrl = 'https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart/0/artists';
-let topAlbumnUrl = 'https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart/0/albums';
+window.addEventListener('load', async function () {
+  let topTracksUrl = 'https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart/0/tracks';
+  let topArtistUrl = 'https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart/0/artists';
+  let topAlbumnUrl = 'https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart/0/albums';
 
-let topTrackslocal = 'js/topTrack.json';
-let topArtistlocal = 'js/topArtist.json';
-let topAlbumnlocal = 'js/topAlbum.json';
+  let topTrackslocal = 'js/topTrack.json';
+  let topArtistlocal = 'js/topArtist.json';
+  let topAlbumnlocal = 'js/topAlbum.json';
 
-
-async function RealizarPeticion(url,mensajeError){
-  try {
-    let response=await fetch(url)
-    let respuesta=await response.json()
-    return respuesta
-  } catch (error) {
+  async function RealizarPeticion(url, mensajeError) {
+    try {
+      let response = await fetch(url)
+      let respuesta = await response.json()
+      return respuesta
+    } catch (error) {
       console.log(`Ocurrio un error en ${mensajeError}, ${error}`);
+    }
   }
-}
 
+  async function loadApis() {
 
-async function loadApis() {
+    let topTracks = await RealizarPeticion(topTracksUrl, 'TopTracks');
+    let topArtists = await RealizarPeticion(topArtistUrl, 'TopArtist');
+    let topAlbums = await RealizarPeticion(topAlbumnUrl, 'TopAlbums');
 
-  let topTracks = await RealizarPeticion(topTracksUrl,'TopTracks');
-  let topArtists = await RealizarPeticion(topArtistUrl,'TopArtist');
-  let topAlbums = await RealizarPeticion(topAlbumnUrl,'TopAlbums');
+    createTopTracks(topTracks);
+    createTopArtists(topArtists);
+    createTopAlbums(topAlbums);
 
-  createTopTracks(topTracks);
-  createTopArtists(topArtists);
-  createTopAlbums(topAlbums);
+  }
 
-}
+  function createTopTracks(track) {
 
-
-function createTopTracks(track) {
-
-  track.data.map(item=>{
-    const article = document.createElement('article');
-    article.className = 'card';
-    article.innerHTML = `
+    track.data.map(item => {
+      const article = document.createElement('article');
+      article.className = 'card';
+      article.innerHTML = `
         <div class="avatar" style="background-image: url(${item.album.cover_small}"></div>
         <div class="informacion">
           <h1>${item.title}</h1>
@@ -49,59 +47,52 @@ function createTopTracks(track) {
         </div>
         `;
 
-    document.getElementById('topTracks').appendChild(article);
+      document.getElementById('topTracks').appendChild(article);
 
 
-  })
+    })
 
-}
+  }
 
 
-function createTopArtists(data) {
-  for (let i = 0; i < data.data.length; i++) {
-    let id=data.data[i].id
-    //let title = data.data[i].picture;
-    let image = data.data[i].picture_small;
-    let artistName = data.data[i].name;
-    let position = data.data[i].position;
-    //console.log("title" + title + "cover" + cover + "artistName"+ artistName+ "album" + album);
-    //${icon}
-    const article = document.createElement('article');
-    article.className = 'card';
-    article.innerHTML = `<div class="avatar" style="background-image: url(${image})"></div>
+  function createTopArtists(data) {
+    for (let i = 0; i < data.data.length; i++) {
+      let id = data.data[i].id
+      //let title = data.data[i].picture;
+      let image = data.data[i].picture_small;
+      let artistName = data.data[i].name;
+      let position = data.data[i].position;
+      //console.log("title" + title + "cover" + cover + "artistName"+ artistName+ "album" + album);
+      //${icon}
+      const article = document.createElement('article');
+      article.className = 'card';
+      article.innerHTML = `<div class="avatar" style="background-image: url(${image})"></div>
         <div class="informacion">
           <h1>${artistName}</h1>
           <h2 id=infoalbum>
             Posición ${position}</h2>
-           <a href='./detail-artist.html?Id=${id}' class="btn">Ver más</a>
-          
+           <button name='artista' value='./detail-artist.html?Id=${id}' class="btn">Ver más</a>
         </div>
         `;
-
-
-
-    document.getElementById('topArtists').appendChild(article);
+      document.getElementById('topArtists').appendChild(article);
+    }
 
   }
 
-}
 
+  function createTopAlbums(data) {
+    for (let i = 0; i < data.data.length; i++) {
 
-
-
-function createTopAlbums(data) {
-  for (let i = 0; i < data.data.length; i++) {
-
-    //let title = data.data[i].picture;
-    let image = data.data[i].cover_small;
-    let album = data.data[i].title;
-    let artist = data.data[i].artist.name;
-    let position = data.data[i].position;
-    //console.log("title" + title + "cover" + cover + "artistName"+ artistName+ "album" + album);
-    //${icon}
-    const article = document.createElement('article');
-    article.className = 'card';
-    article.innerHTML = `<div class="avatar" style="background-image: url(${image})"></div>
+      //let title = data.data[i].picture;
+      let image = data.data[i].cover_small;
+      let album = data.data[i].title;
+      let artist = data.data[i].artist.name;
+      let position = data.data[i].position;
+      //console.log("title" + title + "cover" + cover + "artistName"+ artistName+ "album" + album);
+      //${icon}
+      const article = document.createElement('article');
+      article.className = 'card';
+      article.innerHTML = `<div class="avatar" style="background-image: url(${image})"></div>
         <div class="informacion">
           <h1>${album}</h1>
           <h2 id=infoalbum>
@@ -112,33 +103,34 @@ function createTopAlbums(data) {
             <button class="btn" type="submit">Ver más</button>
           </form>
         </div>`;
-    document.getElementById('topAlbums').appendChild(article);
+      document.getElementById('topAlbums').appendChild(article);
+
+    }
+    /* <article class="card">
+    <div class="avatar" style="background-image: url(${image})"></div>
+    <div class="informacion">
+      <h1>${album}</h1>
+      <h2 id=infoalbum>
+        {artist} </h2>
+      <h3 id=infosingle>
+        Año:2020 </h3>
+      <form method="get" action="detail-album.html">
+        <button class="btn" type="submit">Ver más</button>
+      </form>
+    </div>
+    
+    </article> */
 
   }
-  /* <article class="card">
-  <div class="avatar" style="background-image: url(${image})"></div>
-  <div class="informacion">
-    <h1>${album}</h1>
-    <h2 id=infoalbum>
-      {artist} </h2>
-    <h3 id=infosingle>
-      Año:2020 </h3>
-    <form method="get" action="detail-album.html">
-      <button class="btn" type="submit">Ver más</button>
-    </form>
-  </div>
-  
-  </article> */
 
-}
+
+  await loadApis();
+
+  document.addEventListener('click', (e) => {
+    if (e.target.classList.contains('btn'))
+          location.href = e.target.value
+  })
 
 
 
-
-
-
-loadApis();
-/*
-for(var i=0; i<data.length; i++){
-    console.log("" + data[i].album.title);
-} */
+})
