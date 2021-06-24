a1906e7d344d6d58ffc5b66b739b51b900aa20e5
Serenasaguier <ssaguier@udesa.edu.ar>
2021-06-24 12:35:05 -0300

    js track

--- JS/detail-track.js
+++ JS/detail-track.js
@@ -1,40 +1,6 @@
 //obtengo el query string
 let queryString = window.location.search
 
-<<<<<<< HEAD
-    //capturo ek elemneto que contieene mi track
-
-//capturo ek elemneto que contieene mi track
-
-
-//atrapo el id del gif que voy a usar
-
-
-// fetch()
-//.then(function(respuesta){
-   // return respuesta.json();
-})
-//.then(function(datos){
-  //  let detalles = 
-//})
-
-
-
-//recupero el storage a ver si hay o no fav
-
-
-//si es que hay entro del storage favs
-
-    //transformo el string en un array y le asigno el array a la variable favs
-    
-
-        //se cambio el texto del link favoritos
-        
-
-//Formularios
- //elementos html donde iran los datos de la alet trends = document.querySelector()
- 
-=======
 //paso de ese texto a un objeto
 let objetoQuery = new URLSearchParams(queryString);
 
@@ -118,4 +84,3 @@ botonPlay.addEventListener("click", function(){
 
     })
   })
->>>>>>> b8235262164036f9152009bbbb11d6f791f67dc7
